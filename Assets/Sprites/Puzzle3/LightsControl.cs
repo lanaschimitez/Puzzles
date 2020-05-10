8c0e65c4b9d529be308f5d6fbc849f6c6e347081
lanaschimitez <lana.schimitez@gmail.com>
2020-05-10 14:05:25 -0300

    Config luzes

---
+++ Assets/Sprites/Puzzle3/LightsControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Experimental.Rendering.Universal;
+using Light2D = UnityEngine.Experimental.Rendering.Universal.Light2D;
+
+public class LightsControl : MonoBehaviour
+{
+    public bool lightsOn;
+    public Light2D luz;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightsOn)
+        {
+            luz.enabled = true;
+        }
+        else
+        {
+            luz.enabled = false;
+        }
+    }
+}
