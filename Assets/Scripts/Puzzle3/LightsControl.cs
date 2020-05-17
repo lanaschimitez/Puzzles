666e4f135bbfb95ca480b9792cceaef17db7846d
lanaschimitez <lana.schimitez@gmail.com>
2020-05-16 21:26:59 -0300

    Puzzle 3

---
+++ Assets/Scripts/Puzzle3/LightsControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Experimental.Rendering.Universal;
+using Light2D = UnityEngine.Experimental.Rendering.Universal.Light2D;
+using UnityEngine.UI;
+
+public class LightsControl : MonoBehaviour
+{
+    public bool lightsOn;
+    public Light2D luz;
+    public Button button;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        luz.enabled = (lightsOn) ? true :  false; //boolean para ativar a luz
+        button.GetComponent<Image>().color = Color.red;
+
+    }
+}
